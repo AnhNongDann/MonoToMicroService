36c081137c8ba8a429e71356a1db952179a82e36
AnhNongDan <trung10052014@gmail.com>
2023-11-06 02:44:16 +0700

    Microservices application

--- set_env.sh
+++ set_env.sh
@@ -1,9 +1,9 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
-export POSTGRES_USERNAME=trung
-export POSTGRES_PASSWORD=Anhnongdan123
+export POSTGRES_USERNAME=trunghq5
+export POSTGRES_PASSWORD=as123456
 export POSTGRES_HOST=mydatabase-instance.c31xxjlwyget.us-east-1.rds.amazonaws.com
-export POSTGRES_DB=first_test_db
+export POSTGRES_DB=first_db
 export AWS_BUCKET=project320231104
 export AWS_REGION=us-east-1
 export AWS_PROFILE=default
